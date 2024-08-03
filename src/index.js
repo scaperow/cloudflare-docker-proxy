31615f65afd8a57356d7617fdfa5d20458f9c2b2
Haibo Hu <scaperow@hotmail.com>
2024-08-03 18:21:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.scaperow.com": dockerHub,
+  "quay.scaperow.com": "https://quay.io",
+  "gcr.scaperow.com": "https://gcr.io",
+  "k8s-gcr.scaperow.com": "https://k8s.gcr.io",
+  "k8s.scaperow.com": "https://registry.k8s.io",
+  "ghcr.scaperow.com": "https://ghcr.io",
+  "cloudsmith.scaperow.com": "https://docker.cloudsmith.io",
+  "ecr.scaperow.com": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
